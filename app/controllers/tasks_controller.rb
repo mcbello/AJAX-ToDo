5a9c65f533e295d5f3562908da6e265d122c7692
Monica Bello <mc_bello@bellsouth.net>
2017-08-12 22:11:13 -0400

    convert to AJAX

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,8 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
+    @task = Task.new
+    sleep 1
   end
 
   # GET /tasks/1
@@ -28,7 +30,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        #format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
