29e3060c20ae90119cf3d75d2771d2c614993215
Monica Bello <mc_bello@bellsouth.net>
2017-08-20 22:14:00 -0400

    turbolinks and jquery

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,4 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function() {
+  console.log('document is ready!', new Date()); // this logging is optional
+  $('#task_name').focus();
+});
